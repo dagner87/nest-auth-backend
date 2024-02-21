8e6a7a5ca5a93e739f851cc7e936839677dccad1
Dagner Alena Guerra <dalenag87@gmail.com>
2024-02-21 13:16:40 -0500

    Primer Commit

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -20,6 +20,16 @@ export class User {
     @Prop({ type: [String], default: ['user'] })
     roles: string[];
 
+    /* imageSrc: 'assets/img/avatars/1.jpg',
+    firstName: 'Short',
+    lastName: 'Lowe',
+    street: '548 Cypress Avenue',
+    zipcode: 5943,
+    city: 'Temperanceville',
+    phoneNumber: '+11 (977) 574-3636',
+    mail: 'short.lowe@yourcompany.ca',
+    labels: */
+
 }
 
 
